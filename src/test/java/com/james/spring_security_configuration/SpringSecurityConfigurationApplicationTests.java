10ed6f71bef3fd007ee04614dbdaac735008a002
Anayochukwu James <anayochukwujames11@gmail.com>
2024-12-03 11:22:09 +0100

    Initial commit

---
+++ src/test/java/com/james/spring_security_configuration/SpringSecurityConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.spring_security_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
